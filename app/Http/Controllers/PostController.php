12b8e8e5ab961783ec5f24270726a900db2e78b9
Yuki-gitgit <132742076+Yuki-gitgit@users.noreply.github.com>
2023-05-14 17:26:52 +0900

    Revert "blog投稿一覧表示"

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Post;
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
 public function index(Post $post)
    {
-    return view('posts/index')->with(['posts'=> $post->getPaginateByLimit(1)]);
+    return $post->get();
    }
 }
