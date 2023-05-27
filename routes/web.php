3d49b138580523d019c4a66764c623fc8c010b12
Yuki-gitgit <c200494@mail.ryukoku.ac.jp>
2023-05-27 23:18:22 +0900

    9-2完成

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\CategoryController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,4 +21,5 @@ Route::get('/posts/{post}', [PostController::class, 'show']);
 Route::post('/posts', [PostController::class, 'store']);
 Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
 Route::put('posts/{post}', [PostController::class, 'update']);
-ROute::delete('/posts/{post}', [PostController::class, 'delete']);
+Route::delete('/posts/{post}', [PostController::class, 'delete']);
+Route::get('/categories/{category}' , [CategoryController::class,'index']);
